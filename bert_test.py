e468c53e7e47d32cbcd95907fc404d73ccaf69db
zzp <zzpmiracle@gmail.com>
2019-06-12 21:04:01 +0800

    dataset

--- bert_test.py
+++ bert_test.py
@@ -2,27 +2,40 @@ import csv
 from elasticsearch import Elasticsearch
 import json
 from get_topics import get_topics
+import numpy
 
 def process_doc(doc):
+    if not doc['title']:
+        return doc['contents']
     doc_string = doc['title']+'.'+doc['contents']
     doc_string = doc_string.replace('\n','').replace('\t','')
     return doc_string
 def create_dataset():
     doc_dic = get_topics()
     gains_file = 'D:\\news_track\\bqrels.exp-gains.txt'
+    dst_file_path = 'D:\\news_track\\data.txt'
+
     # es = Elasticsearch('localhost:9200')
     es = Elasticsearch('222.20.24.230:9200')
-    with open(gains_file) as gf:
+    ids = set()
+    with open(gains_file) as gf,open(dst_file_path,'w',encoding='utf-8') as df:
         reader = csv.reader(gf, delimiter=" ")
         lines = []
         for line in reader:
             id1 = doc_dic[int(line[0])]
             id2 = line[2]
-            doc1 = es.get(index='news', id=id1)['_source']
-            doc2 = es.get(index='news', id=id2)['_source']
-            doc1 = process_doc(doc1)
-            doc2 = process_doc(doc2)
-            lines.append(doc1+'\t'+doc2+'\t'+line[-1])
-    print(lines)
+            df.write(','.join([id1, id2, line[-1]]))
+            ids.add(id1)
+            ids.add(id2)
+        articles = {}
+        for id in ids:
+            articles['id'] = process_doc(es.get(index='news',id=id)['_source'])
+            # lines.append(doc1+','+doc2+','+line[-1])
+            # lines.append([doc1,doc2,line[-1]])
+'''
+随机选取两个id  相关度-1
+保存到ids 和 articles
+'''
+
 if __name__ == "__main__":
     create_dataset()
